37db30d4740adaf1ac467df61e2db0414be5fce3
Elena <estamenkovskaa@gmail.com>
2024-07-27 14:56:01 +0200

    Add Postgres database

--- OnlineLibrary/Online-Library.Repository/ApplicationDbContext.cs
+++ OnlineLibrary/Online-Library.Repository/ApplicationDbContext.cs
@@ -4,16 +4,11 @@ using Online_Library.Domain.Entities;
 
 namespace Online_Library.Repository
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<User> Users { get; set; }
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
 }
---
+++ OnlineLibrary/Online-Library.Repository/DesignTimeDbContextFactory.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Online_Library.Repository;
+
+namespace Online_Library.Repository;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Ensure the base path is correct
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.db.json")
+            .Build();
+
+        // Retrieve the connection string
+        var connectionString = configuration.GetConnectionString("OnlineLibraryDBConnectionString");
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
