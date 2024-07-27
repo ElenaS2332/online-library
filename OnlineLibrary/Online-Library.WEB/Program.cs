12ff7f62435b95f306426716470ff7f842cd6b53
Elena <estamenkovskaa@gmail.com>
2024-07-27 13:28:25 +0200

    Configure services and repositories in Program.cs

--- OnlineLibrary/Online-Library.WEB/Program.cs
+++ OnlineLibrary/Online-Library.WEB/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Online_Library.Repository;
+using Online_Library.Repository.Implementations;
+using Online_Library.Repository.Interfaces;
+using Online_Library.Service.Implementations;
+using Online_Library.Service.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +18,17 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IAuthorsRepository, AuthorsRepository>();
+builder.Services.AddScoped<IBooksRepository, BooksRepository>();
+builder.Services.AddScoped<IGenresRepository, GenresRepository>();
+
+builder.Services.AddTransient<IUsersService, UsersService>();
+builder.Services.AddTransient<IAuthorsService, AuthorsService>();
+builder.Services.AddTransient<IBooksService, BooksService>();
+builder.Services.AddTransient<IGenresService, GenresService>();
+
 
 var app = builder.Build();
 
